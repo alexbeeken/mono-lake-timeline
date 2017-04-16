35106613be00edc20159a85a1b5642ea3a1a7817
Alex Beeken <alexbeeken@Alexs-MacBook-Pro.local>
2017-04-15 22:11:32 -0700

    ,

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Control from './components/control'
 import './styles/index.css'
+import "../node_modules/react-vis/dist/style.css";
 
 class App extends Component {
   render() {
